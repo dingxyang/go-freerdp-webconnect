c49551df413f7f9d77c915fa0561d3c7b0b95477
xydinga <>
2026-03-31 19:27:40 +0800

    fix: 添加日志文件

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gofreerdp
 go 1.24
 
 require (
+	github.com/tea4go/gh v1.3.4
 	github.com/wailsapp/wails/v2 v2.11.0
 	golang.org/x/net v0.35.0
 )
@@ -34,6 +35,8 @@ require (
 require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/shiena/ansicolor v0.0.0-20230509054315-a9deabde6e02 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
--- main.go
+++ main.go
@@ -5,11 +5,18 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
+	"gofreerdp/backend"
+
+	logs "github.com/tea4go/gh/log4go"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
-	"gofreerdp/backend"
 )
 
 //go:embed frontend/dist
@@ -21,7 +28,26 @@ var appVer string = "0.0.2"      // 应用版本
 var IsBeta string                // 是否为 Beta 版本，由构建注入
 var BuildTime string             // 构建时间，由构建注入
 
+func filepathJoin(elem ...string) string {
+	path := filepath.Join(elem...)
+	if runtime.GOOS == "windows" {
+		return strings.ReplaceAll(path, "\\", "/")
+	}
+	return path
+}
+
 func main() {
+
+	//#region 处理日志
+	logs_file_name := filepathJoin(os.TempDir(), "ulog_qrdp.log")
+	logs.SetLogger("file", fmt.Sprintf(`{"filename":"%s", "perm": "0666"}`, logs_file_name))
+	logs.Notice("Start Application - %s Build:%s", appVer, BuildTime)
+	logs.StartLogger()
+
+	logs.Notice("= 日志级别 = console %s", logs.GetLevelName(logs.GetLevel("console")))
+	logs.Notice("= 日志级别 = file    %s", logs.GetLevelName(logs.GetLevel("file")))
+	logs.Notice("= 日志文件 = %s", logs_file_name)
+
 	app := backend.NewApp(appVer)
 
 	err := wails.Run(&options.App{
